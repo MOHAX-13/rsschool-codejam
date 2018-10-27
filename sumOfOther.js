39479aed94ba3316e90c5fbfe4a8363d5522df86
DENIS <MOHAX1983@gmail.com>
2018-10-27 22:23:26 +0300

    feat(add): add sumOfOther.js

---
+++ sumOfOther.js
@@ -0,0 +1,11 @@
+function sumOfOther(arr) {
+	const result = [];
+	const inputArr = arr;
+	for (let i = 0, len = inputArr.length; i < len; i++) {
+		var temp = inputArr.reduce(function(a, b) {
+			return a + b;
+		});
+		result.push(temp-inputArr[i]);
+	};
+    return result;
+};
