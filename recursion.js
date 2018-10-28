9b8104fe2ad0485d39a04ea7b6a7a81c3b85e351
DENIS <MOHAX1983@gmail.com>
2018-10-28 17:22:59 +0300

    feat(add): add recursion.js

---
+++ recursion.js
@@ -0,0 +1,23 @@
+let tree = {"value":100,"left":{"value":90,"left":{"value":70},"right":{"value":99}},"right":{"value":120,"left":{"value":110},"right":{"value":130}}};
+
+function recursion(tree) {
+  const array = [];
+  let nodeLevel = 0;
+  (function stepNode(node) {
+    if (node.left) {
+      nodeLevel += 1;
+      stepNode(node.left);
+      nodeLevel -= 1;
+    }
+    if (node.right) {
+      nodeLevel += 1;
+      stepNode(node.right);
+      nodeLevel -= 1;
+    }
+    if (!array[nodeLevel]) {
+      array[nodeLevel] = [];
+    }
+    array[nodeLevel].push(node.value);
+  }(tree));
+  return array;
+};
